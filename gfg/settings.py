f6e922fef3af12dc545ba8618a4d9941789f3627
kavyp12 <151881125+vivekrp11@users.noreply.github.com>
2024-02-24 15:38:21 +0530

    commit

--- gfg/settings.py
+++ gfg/settings.py
@@ -71,9 +71,6 @@ DATABASES = {
     }
 }
 
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
-CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
 
 import os 
 
